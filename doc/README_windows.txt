831340dbd8bc6663f60cd073b5264cc5e43d7a28
sherlockcoin <sherlockcoin@gmail.com>
2016-09-26 22:22:47 +0530

    Initial Commit - Original Source

---
+++ doc/README_windows.txt
@@ -0,0 +1,17 @@
+ion Core 1.0.0
+==============
+
+Intro
+-----
+ion is a free open source project derived from Bitcoin, with
+the goal of providing a long-term energy-efficient scrypt-based crypto-currency.
+Built on the foundation of Bitcoin, PPCoin and NovaCoin, innovations such as proof-of-stake and scrypt
+help further advance the field of crypto-currency.
+
+Setup
+-----
+Unpack the files into a directory and run ion-qt.exe.
+
+See the ion wiki at:
+  http://ionomy.wiki/
+for more help and information.
---
+++ src/qt/locale/ion_vi.ts
@@ -0,0 +1,168 @@
+<TS language="vi" version="2.0">
+<context>
+    <name>AboutDialog</name>
+    </context>
+<context>
+    <name>AddEditionNode</name>
+    </context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Double-click to edit address or label</source>
+        <translation>Click đúp chuột để chỉnh sửa địa chỉ hoặc nhãn dữ liệu</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Tạo một địa chỉ mới</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Sao chép các địa chỉ đã được chọn vào bộ nhớ tạm thời của hệ thống</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Xóa</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Tập tin tách biệt bởi dấu phẩy (*.csv)</translation>
+    </message>
+    </context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Nhãn dữ liệu</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Địa chỉ</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(chưa có nhãn)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    </context>
+<context>
+    <name>BitcoinGUI</name>
+    </context>
+<context>
+    <name>ClientModel</name>
+    </context>
+<context>
+    <name>CoinControlDialog</name>
+    <message>
+        <source>Amount</source>
+        <translation>Số lượng</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Địa chỉ</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(chưa có nhãn)</translation>
+    </message>
+    </context>
+<context>
+    <name>DarksendConfig</name>
+    </context>
+<context>
+    <name>EditAddressDialog</name>
+    </context>
+<context>
+    <name>GUIUtil::HelpMessageBox</name>
+    </context>
+<context>
+    <name>I2POptionsWidget</name>
+    </context>
+<context>
+    <name>MasternodeManager</name>
+    </context>
+<context>
+    <name>OptionsDialog</name>
+    </context>
+<context>
+    <name>OverviewPage</name>
+    </context>
+<context>
+    <name>PaymentServer</name>
+    </context>
+<context>
+    <name>QRCodeDialog</name>
+    </context>
+<context>
+    <name>RPCConsole</name>
+    </context>
+<context>
+    <name>SendCoinsDialog</name>
+    <message>
+        <source>(no label)</source>
+        <translation>(chưa có nhãn)</translation>
+    </message>
+    </context>
+<context>
+    <name>SendCoinsEntry</name>
+    </context>
+<context>
+    <name>ShowI2PAddresses</name>
+    </context>
+<context>
+    <name>SignVerifyMessageDialog</name>
+    </context>
+<context>
+    <name>TrafficGraphWidget</name>
+    </context>
+<context>
+    <name>TransactionDesc</name>
+    <message>
+        <source>Amount</source>
+        <translation>Số lượng</translation>
+    </message>
+    </context>
+<context>
+    <name>TransactionDescDialog</name>
+    </context>
+<context>
+    <name>TransactionTableModel</name>
+    <message>
+        <source>Address</source>
+        <translation>Địa chỉ</translation>
+    </message>
+    <message>
+        <source>Amount</source>
+        <translation>Số lượng</translation>
+    </message>
+    </context>
+<context>
+    <name>TransactionView</name>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Tập tin tách biệt bởi dấu phẩy (*.csv)</translation>
+    </message>
+    <message>
+        <source>Label</source>
+        <translation>Nhãn dữ liệu</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Địa chỉ</translation>
+    </message>
+    <message>
+        <source>Amount</source>
+        <translation>Số lượng</translation>
+    </message>
+    </context>
+<context>
+    <name>WalletModel</name>
+    </context>
+<context>
+    <name>bitcoin-core</name>
+    </context>
+<context>
+    <name>ionNodeConfigDialog</name>
+    </context>
+</TS>
